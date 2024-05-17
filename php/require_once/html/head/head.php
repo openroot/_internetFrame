66ea3d117e86b6e35eee8abfd738ad2b7e294914
Debaprasad Tapader <dev.openroot@gmail.com>
2024-05-17 19:19:22 +0530

    update

---
+++ php/require_once/html/head/head.php
@@ -0,0 +1,13 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+	<meta name="generator" content="Hugo 0.84.0">
+	<title>Bootstrap Template · Bootstrap v5.0</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/custom.css" rel="stylesheet">
+</head>
---
+++ php/require_once/html/html.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html lang="en">
+<?php
+	require_once("head/head.php");
+	require_once("body/body.php");
+?>
+</html>
