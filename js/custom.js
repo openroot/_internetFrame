75a47dc1ce3464cbd03b1da91ed5dfd9d84f1392
Debaprasad Tapader <dev.openroot@gmail.com>
2024-05-19 03:11:28 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,13 @@
+/* JavaScript */
+
 window.addEventListener("resize", function () {
 	// window.location.reload(); // Required for auto-setting navigation menu.
 });
 
 document.addEventListener("DOMContentLoaded", function () {
 	// Prevent closing from click inside dropdown
-	document.querySelectorAll('.dropdown-menu').forEach(function (element) {
-		element.addEventListener('click', function (e) {
+	document.querySelectorAll(".dropdown-menu").forEach(function (element) {
+		element.addEventListener("click", function (e) {
 			e.stopPropagation();
 		});
 	})
@@ -13,34 +15,34 @@ document.addEventListener("DOMContentLoaded", function () {
 	// Make it as accordion for smaller screens
 	if (window.innerWidth < 992) {
 		// Close all inner dropdowns when parent is closed
-		document.querySelectorAll('.navbar .dropdown').forEach(function (everydropdown) {
-			everydropdown.addEventListener('hidden.bs.dropdown', function () {
+		document.querySelectorAll(".navbar .dropdown").forEach(function (everydropdown) {
+			everydropdown.addEventListener("hidden.bs.dropdown", function () {
 				// After dropdown is hidden, then find all submenus
-				this.querySelectorAll('.submenu').forEach(function (everysubmenu) {
+				this.querySelectorAll(".submenu").forEach(function (everysubmenu) {
 					// Hide every submenu as well
-					everysubmenu.style.display = 'none';
+					everysubmenu.style.display = "none";
 				});
-				this.querySelectorAll('.submenu-left').forEach(function (everysubmenu) {
+				this.querySelectorAll(".submenu-left").forEach(function (everysubmenu) {
 					// Hide every submenu as well
-					everysubmenu.style.display = 'none';
+					everysubmenu.style.display = "none";
 				});
-				this.querySelectorAll('.submenu-up').forEach(function (everysubmenu) {
+				this.querySelectorAll(".submenu-up").forEach(function (everysubmenu) {
 					// Hide every submenu as well
-					everysubmenu.style.display = 'none';
+					everysubmenu.style.display = "none";
 				});
 			})
 		});
-		document.querySelectorAll('.dropdown-menu a').forEach(function (element) {
-			element.addEventListener('click', function (e) {
+		document.querySelectorAll(".dropdown-menu a").forEach(function (element) {
+			element.addEventListener("click", function (e) {
 				let nextEl = this.nextElementSibling;
-				if (nextEl && (nextEl.classList.contains('submenu') || nextEl.classList.contains('submenu-left') || nextEl.classList.contains('submenu-up'))) {
+				if (nextEl && (nextEl.classList.contains("submenu") || nextEl.classList.contains("submenu-left") || nextEl.classList.contains("submenu-up"))) {
 					// Prevent opening link if link needs to open dropdown
 					e.preventDefault();
 					console.log(nextEl);
-					if (nextEl.style.display == 'block') {
-						nextEl.style.display = 'none';
+					if (nextEl.style.display == "block") {
+						nextEl.style.display = "none";
 					} else {
-						nextEl.style.display = 'block';
+						nextEl.style.display = "block";
 					}
 
 				}
@@ -51,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-// jQuery
+/* jQuery */
 $(document).ready(function(){
 	$("p").click(function(){
 		//$(this).hide();
