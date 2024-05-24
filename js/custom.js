fa90f6e43f63fc69db661456d9b1ba9d7f32956a
Debaprasad Tapader <dev.openroot@gmail.com>
2024-05-24 22:00:37 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -93,7 +93,7 @@ function setTheme(themeIndex) {
 						},
 						"background-color": {
 							"background": ["body", "header>div#navbarTopShade", "footer>div#navbarBottomShade"],
-							"base": [".navbar", "input", "textarea", ".form-control", ".form-select", ".card-body", "li>.dropdown-menu"],
+							"base": [".navbar", ".dropdown-menu", "input", "textarea", ".form-control", ".form-select", ".card-body", "li>.dropdown-menu"],
 							"focusPrime": [".btn-primary", ".btn-outline-primary", ".card-header"],
 							"transparent": [".btn-outline-primary", "input[type='range']"]
 						},
