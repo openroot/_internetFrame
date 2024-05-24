56cefab7f41a44cf5c9de472340d96bbb110cbfc
Debaprasad Tapader <dev.openroot@gmail.com>
2024-05-24 07:22:43 +0530

    update

--- php/require_once/html/body/main/go/rough/rough.php
+++ php/require_once/html/body/main/go/rough/rough.php
@@ -14,7 +14,7 @@
 	<h1>Navbar example</h1>
 	<p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works.
 		As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
-	<a class="btn btn-primary btn-lg" href="../components/navbar/" role="button">View navbar docs »</a>
+	<a class="btn btn-primary btn-lg" role="button">View navbar docs »</a>
 </div>
 <!-- end -->
 
